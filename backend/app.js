9e2cc5d460d455b6c4916b3fe6f3b3b89ff32b11
Dmitry Tupikin <dilondd11@gmail.com>
2025-06-08 13:02:41 +0300

    fix deploy

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import hotelRoutes from "./routes/hotels.js";
 import roomRoutes from "./routes/rooms.js";
@@ -15,7 +17,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -27,14 +29,19 @@ mongoose
   .then(() => console.log("MongoDB подключен"))
   .catch((error) => console.error("Ошибка подключения MongoDB: ", error));
 
-app.get("/", (req, res) => {
-  res.send("Сервер работает");
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const frontendPath = path.join(__dirname, "../frontend/dist");
+app.use(express.static(frontendPath));
 
 app.use("/hotels", hotelRoutes);
 app.use("/rooms", roomRoutes);
 app.use("/bookings", bookingRoutes);
 app.use("/auth", authRoutes);
+
+app.get(/(.*)/, (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
